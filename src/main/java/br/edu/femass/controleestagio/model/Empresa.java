65e3eab944f9d3f50d028bc929f49ce843f8fd7f
dumas <dumas@x230>
2019-10-15 06:16:03 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/femass/controleestagio/model/Empresa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.femass.controleestagio.model;
+
+/**
+ *
+ * @author dumas
+ */
+public class Empresa {
+
+    private Integer idEmpresa;
+    private String nomeEmpresa;
+    private String cNPJ;
+    private String contatoDoRH;
+    private String eMail;
+    private String Rua;
+    private Integer numero;
+    private String compplemento;
+    private String cEP;
+
+}
---
+++ src/main/java/br/edu/femass/controleestagio/model/Estagio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.femass.controleestagio.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author dumas
+ */
+public class Estagio {
+
+    private Aluno alunoEstagio;
+    private ProfessorOrientador orientadorEstagio;
+    private LocalDate dataInicioEstagio;
+    private LocalDate datafimEstagio;
+    private Status statusDoEstagio;
+
+}
